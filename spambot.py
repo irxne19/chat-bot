472a4c7c032e90190c8b76e12ad29277e8782618
irxne19 <68653473+irxne19@users.noreply.github.com>
2020-11-18 10:11:23 +0530

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -11,8 +11,8 @@ def job():
     pyautogui.press("enter")
 
 
-schedule.every(1).minutes.do(job)
-
+schedule.every(1).minutes.do(job) # This will send a text every 1 minute
+schedule.every().day.at("10:30").do(job) # This will send a message at 10:30 am everyday as long as program is running and The chat is kept open
 
 while True:
     schedule.run_pending()
